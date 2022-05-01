cee47a5c0e617b47c7dc25b3947afb6cc76f0b57
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2022-05-01 12:31:27 -0300

    Started cadClient component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { InicioComponent } from './inicio/inicio.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { CadClienteComponent } from './cad-cliente/cad-cliente.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'inicio', component: InicioComponent},
+  {path: 'cadCliente', component: CadClienteComponent},
 ];
 
 @NgModule({
